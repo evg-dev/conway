d6c65d3a23cc46c8ba48952380045eb0a664b9e7
Evgeny <evg.develop@gmail.com>
2022-01-24 22:07:18 +0500

    create game loop, pause, exit and delay

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,7 +2,6 @@ module Lib
     ( game
     ) where
 
-
 import UI.NCurses
 
 data State
@@ -10,22 +9,69 @@ data State
   | Init
   | Pause
 
+delay :: Integer
+delay = 500 -- milliseconds
+
+getDelay :: Bool -> Maybe Integer
+getDelay d
+    | d == False = Nothing
+    | d == True = Just delay
+
 game :: IO ()
 game = runCurses $ do
     (hw, ww) <- screenSize -- (height window,  width window)
     setEcho False
-    setCursorMode CursorInvisible
+    setCursorMode CursorInvisible -- hide cursor
     w <- defaultWindow
-    loop w Init hw ww
+    let с = 0
+    loop w Init hw ww с False
 
-
-loop :: Window -> State -> Integer -> Integer -> Curses ()
-loop w Init hw ww = do
+-- main game loop
+loop :: Window -> State -> Integer -> Integer ->  Integer -> Bool -> Curses ()
+loop w Init hw ww generation is_cycle = do
     updateWindow w $ do
         renderBorder hw ww
+        moveCursor 0 0
+        drawString $ show generation
     render
-    waitForExit w (\ev -> ev == EventCharacter 'q' || ev == EventCharacter 'Q')
+    waitForNext w Init hw ww generation is_cycle
 
+
+
+waitForNext :: Window -> State -> Integer -> Integer ->  Integer -> Bool -> Curses ()
+waitForNext w Init hw ww generation is_cycle = do
+    let current_delay = getDelay is_cycle
+    event <- getEvent w current_delay
+    let action      | (event ==  Just (EventCharacter 'q')) = do      -- exit
+                        updateWindow w $ do
+                            moveCursor 0 0
+                            drawString $ show "q"
+                        render
+                        return ()
+
+                    | (event ==  Just (EventCharacter 'p')) && (is_cycle == True) = do     -- pause
+                        updateWindow w $ do
+                            moveCursor 0 0
+                            drawString $ show "pause"
+                        render
+                        loop w Init hw ww (generation) False
+
+                    | (event ==  Just (EventCharacter 'p')) && (is_cycle == False) = do     -- resume
+                        updateWindow w $ do
+                            moveCursor 0 0
+                            drawString $ show "resume"
+                        render
+                        loop w Init hw ww (generation + 1) True
+
+                    | (event ==  Just (EventCharacter 'n')) && (is_cycle == False) = do      -- manual next
+                        loop w Init hw ww (generation + 1) False
+
+                    | is_cycle == False = do                                                 -- no cycle, no events - stop
+                        loop w Init hw ww (generation) False
+
+                    | otherwise = do                                                         -- timeout
+                        loop w Init hw ww (generation + 1) True
+    action
 
 renderBorder :: Integer -> Integer -> Update ()
 renderBorder  hw ww = do
@@ -35,12 +81,3 @@ renderBorder  hw ww = do
     drawLineH ( Just glyphStipple) ww
     moveCursor 0 (ww - 1)
     drawLineV ( Just glyphStipple) hw
-
-
-waitForExit :: Window -> (Event -> Bool) -> Curses ()
-waitForExit w p = loop where
-    loop = do
-        ev <- getEvent w Nothing
-        case ev of
-            Nothing -> loop
-            Just ev' -> if p ev' then return () else loop
